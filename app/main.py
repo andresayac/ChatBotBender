4761a6982193bd4b0bd49740b89bf0b80a3cfe4d
andresayac <andresayac@gmail.com>
2020-11-21 03:41:49 -0800

    Primer Commit

---
+++ app/main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+from chatterbot.trainers import ListTrainer
+
+flaskApp = Flask(__name__)
+flaskApp.config['SECRET_KEY'] = 'secret'
+AIBot = ChatBot("Bender")
+trainer = ChatterBotCorpusTrainer(AIBot)
+trainer.train("chatterbot.corpus.spanish")
+
+@flaskApp.route("/")
+def home():    
+    return render_template("home.html") 
+
+@flaskApp.route("/get")
+def get_bot_response():    
+    userMsg = request.args.get('msg')    
+    return str(AIBot.get_response(userMsg)) 
+
+if __name__ == "__main__":    
+  flaskApp.run(host='0.0.0.0')
